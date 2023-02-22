a2c746f684561a5b8fa22a55d7a5533954cd33ae
kanybekova eleonora <eelya250@gmail.com>
2023-02-23 00:34:21 +0600

    made a list in js and styled it

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // 2. У нас есть массив ["hand", "leg", "head", "finger", "eye"] вывести элементы массива в виде списка.
-let body = ["hand", "leg", "head", "finger", "eye"];
+let bodyParts = ["hand", "leg", "head", "finger", "eye"];
 let list = document.createElement("ul");
 document.body.prepend(list);
-for (let i = 0; i < body.length; i++) {
+for (let i = 0; i < bodyParts.length; i++) {
   let listItem = document.createElement("li");
-  listItem.textContent = body[i];
+  listItem.textContent = bodyParts[i];
   list.style.listStyleType = "none";
   listItem.style.fontSize = "20px";
   listItem.style.textTransform = "uppercase";
