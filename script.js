f8dc9cb43ab7479cf06ce21a3278ad6808ba1ed9
kanybekova eleonora <eelya250@gmail.com>
2023-02-23 02:14:31 +0600

    in the third task, I created 3 buttons in js and styled them in css

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ for (let i = 0; i < bodyParts.length; i++) {
   list.style.width = "100px";
   list.append(listItem);
 }
+
+// 3. У нас есть массив ["hi", "hello", "bonjour"] вывести элементы массива в виде кнопок.
+let button = ["hi", "hello", "bonjour"];
+let btn1 = document.createElement("button");
+btn1.className = "button-style";
+btn1.textContent = button[0];
+document.body.append(btn1);
+
+let btn2 = document.createElement("button");
+btn2.textContent = button[1];
+document.body.append(btn2);
+btn2.className = "button-style";
+
+let btn3 = document.createElement("button");
+btn3.textContent = button[2];
+document.body.append(btn3);
+btn3.className = "button-style";
